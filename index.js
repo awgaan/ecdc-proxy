155d30d7821cf560fcb7d06cd77e7efd358c4774
Wahed Mangal <mangal.wahed@gmail.com>
2022-03-06 21:37:06 +0100

    fix: cors issues

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+}
+
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  const request = event.request
+  if (request.method === 'OPTIONS') {
+    event.respondWith(handleOptions(request))
+  } else {
+    event.respondWith(handleRequest(request))
+  }
 })
+
+const handleOptions = async (request) => {
+  let headers = request.headers
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    let respHeaders = {
+      ...corsHeaders,
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers',
+      ),
+    }
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  } else {
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+}
 
 const handleRequest = async (request) => {
   const { pathname, search } = new URL(request.url)
-  const url = `https://opendata.ecdc.europa.eu/${pathname}${search}`
-  const resp = await fetch(url)
+  const target = `https://opendata.ecdc.europa.eu/${pathname}${search}`
+  const resp = await fetch(target)
 
-  const newResponse = new Response(resp.body, resp)
-  newResponse.headers.append('Access-Control-Allow-Origin', '*')
+  const newResponse = new Response(resp.body)
+  newResponse.headers.append('Content-Type', 'application/json')
   newResponse.headers.append(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Origin',
+    request.headers.get('ORIGIN'),
   )
+  newResponse.headers.append('Vary', 'Origin')
 
   return newResponse
 }
